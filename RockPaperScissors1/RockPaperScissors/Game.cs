a5de0ee4f7ea942a5ae19a04f0ca33db4c72d4ec
ahami14 <ahnikkahamilton14@gmail.com>
2019-09-25 13:52:04 -0500

    uploading initial files

---
+++ RockPaperScissors1/RockPaperScissors/Game.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors
+{
+    class Game
+    {
+        //member vairables (Has A)
+
+
+        //constructor (Spawner)
+
+        //member methods (Can do)
+
+
+
+
+        //display rules for game
+        //choose if player would like to verse another player or computer
+        //Allow player to choose Rock, Paper, Scissors, Lizard, or Spock
+        //Allow computer to randomly generate their answer based on a number 1-5
+    }
+}
---
+++ RockPaperScissors1/RockPaperScissors/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors
+{
+    class Player
+    {
+        //member vairables (Has A)
+        int score;
+
+        //constructor (Spawner)
+
+        //member methods (Can do)
+    }
+}
